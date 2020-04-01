b9f753c0f24b6cc36c821e42b80fd815fb65150f
Fitilsan <Fitil_san@mail.ru>
2020-04-01 21:09:48 +0300

    First commit

--- Assets/Scripts/TestingITick/TestCube/CubeScriptableObj.cs
+++ Assets/Scripts/TestingITick/TestCube/CubeScriptableObj.cs
@@ -13,4 +13,18 @@ public class CubeScriptableObj : ScriptableObject
 
     public GameObject Target;
     
+
+    void mat()
+    {
+
+        float maxDistance = 5;
+        Vector3 player = Vector3.zero;
+        Vector3 Enemy = Vector3.zero;
+        var distance = Vector3.SqrMagnitude(player - Enemy);
+
+        if (distance > maxDistance * maxDistance) Debug.Log("Hurrah");
+
+        Vector3.Distance(player, Enemy);
+        Vector3.Magnitude(player);
+    }
 }
